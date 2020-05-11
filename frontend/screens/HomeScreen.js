ff938599a8a5a14b0d42ab9ce72399ba62774922
shubham <35225780+shubham1397@users.noreply.github.com>
2020-05-11 15:50:56 +0530

    Update HomeScreen.js

--- frontend/screens/HomeScreen.js
+++ frontend/screens/HomeScreen.js
@@ -6,21 +6,19 @@ import Modal from 'react-native-modal';
 import { TextInput } from 'react-native-paper';
 import Btn from '../components/Button';
 import { FlatList } from 'react-native-gesture-handler';
-
 import * as ImagePicker from 'expo-image-picker';
 import Constants from 'expo-constants';
 import * as Permissions from 'expo-permissions';
 
 
-export default function HomeScreen({navigation}) {
-  
+export default function HomeScreen({ navigation }) {
+
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
   const [modalData, setModalData] = useState([]);
-
   const [image, setImage] = useState([]);
-  
+
 
 
   useEffect(() => {
@@ -31,9 +29,7 @@ export default function HomeScreen({navigation}) {
     });
 
 
-
-
-  const getPermissionAsync = async () => {
+    const getPermissionAsync = async () => {
       if (Constants.platform.ios) {
         const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
         if (status !== 'granted') {
@@ -41,7 +37,7 @@ export default function HomeScreen({navigation}) {
         }
       }
     };
-  
+
 
     const backAction = () => {
       Alert.alert("Hold on!", "Are you sure you want to go back?", [
@@ -59,165 +55,135 @@ export default function HomeScreen({navigation}) {
       "hardwareBackPress",
       backAction
     );
-
-    return () => backHandler.remove();
-
-
+     return () => backHandler.remove();
   }, []);
 
 
 
   const isAvailable = () => {
-    const timeout = new Promise((resolve, reject) => {
-        setTimeout(reject, 300, 'Request timed out');
+      const timeout = new Promise((resolve, reject) => {
+      setTimeout(reject, 300, 'Request timed out');
     });
 
-    const request = fetch('http://192.168.43.187:3000/crud/');
+      const request = fetch('http://192.168.43.187:3000/crud/');
 
-    return Promise
+      return Promise
         .race([timeout, request])
         .then(response => console.log('It worked :)'))
         .catch(error => console.log('It timed out :('));
-}
-
-
-const getData = () =>{
-
-  isAvailable().then(fetch('http://192.168.43.187:3000/crud/')
-  .then((response) => response.json())
-  .then((json) => {setData(json.data); console.log(json.data) })
-  .catch((error) => console.error  (error))
-  .finally(() => setLoading(false))
-  )}
-
-
-  
-// const data =
-
-// {
-
-// 	"data": [{
-//         name:"Shubham Shinde",
-//         Designation:"Web Developer",
-//         path:'https://reactnative.dev/img/tiny_logo.png'
-// 		},
-// 		{
-//         name:"Sandip Chavan",
-//         Designation:"App Developer",
-//         path:'https://reactnative.dev/img/tiny_logo.png'
-// 		}
-// 	]
-// }
-
- const openModal = (item,index) => {
-  setModalData(item);
-  setModalVisible(!modalVisible)
-  console.log(modalData.name);
- }
-
- const picImage = async () => {
-
-  try {
-    let result = await  ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-    if (!result.cancelled) {
-      setImage(result);
-      console.log("result.uri",result.uri)
-    }
-
-    console.log("result",result);
-  } catch (E) {
-    console.log(E);
   }
 
 
- }
+  const getData = () => {
+    isAvailable().then(fetch('http://192.168.43.187:3000/crud/')
+      .then((response) => response.json())
+      .then((json) => { setData(json.data); console.log(json.data) })
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false))
+    )
+  }
 
 
+  const openModal = (item, index) => {
+    setModalData(item);
+    setModalVisible(!modalVisible)
+    console.log(modalData.name);
+  }
 
+  const picImage = async () => {
 
- const createFormData = (photo, body) => {
-  const data = new FormData();
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+      if (!result.cancelled) {
+        setImage(result);
+        console.log("result.uri", result.uri)
+      }
 
-  data.append("photo", {
-    name: photo.fileName,
-    type: photo.type,
-    uri:
-      Platform.OS === "android" ? photo.uri : photo.uri.replace("file://", "")
-  });
-
-  Object.keys(body).forEach(key => {
-    data.append(key, body[key]);
-  });
-
-  return data;
-};
-
-
-
-
- const updateData = () =>{
-  setModalVisible(!modalVisible)  
-  
-  let fileType = image.uri.substring(image.uri.lastIndexOf(".") + 1);
-
-  console.log("fileType",fileType);
-
-  let formData = new FormData();
-
-  formData.append("photo", {
-    uri:image.uri,
-    name: 'first',
-    type: `image/${fileType}`
-  });
-
-  formData.append('name', String(modalData.name));
-  formData.append('designation', String(modalData.designation));
-
-
-  let options = {
-    method: "PUT",
-    body: formData,
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'multipart/form-data; charset=utf-8; boundary="6ff46e0b6b5148d984f148b6542e5a5d',
-      'Content-Disposition': 'form-data'
+      console.log("result", result);
+    } catch (E) {
+      console.log(E);
     }
+  }
+
+
+  const createFormData = (photo, body) => {
+    const data = new FormData();
+    data.append("photo", {
+      name: photo.fileName,
+      type: photo.type,
+      uri:
+        Platform.OS === "android" ? photo.uri : photo.uri.replace("file://", "")
+    });
+
+    Object.keys(body).forEach(key => {
+      data.append(key, body[key]);
+    });
+    return data;
   };
 
 
 
-  fetch("http://192.168.43.187:3000/crud/update/"+modalData._id,options)
-  .then(() => getData())
-  .catch((error)=> console.error(error))
- }
 
- const deleteData = id =>{
-  Alert.alert(
-    "Delete Data",
-    "Dou you really want to delete "+id,
-    [
-      {
-        text: "Cancel",
-        onPress: () => console.log("Cancel Pressed"),
-        style: "cancel"
-      },
-      { text: "OK", onPress: () => 
-          {
-            fetch('http://192.168.43.187:3000/crud/delete/'+id, {
+  const updateData = () => {
+    setModalVisible(!modalVisible)
+
+    let fileType = image.uri.substring(image.uri.lastIndexOf(".") + 1);
+    console.log("fileType", fileType);
+    let formData = new FormData();
+
+    formData.append("photo", {
+      uri: image.uri,
+      name: 'first',
+      type: `image/${fileType}`
+    });
+
+    formData.append('name', String(modalData.name));
+    formData.append('designation', String(modalData.designation));
+
+
+    let options = {
+      method: "PUT",
+      body: formData,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'multipart/form-data; charset=utf-8; boundary="6ff46e0b6b5148d984f148b6542e5a5d',
+        'Content-Disposition': 'form-data'
+      }
+    };
+
+    fetch("http://192.168.43.187:3000/crud/update/" + modalData._id, options)
+      .then(() => getData())
+      .catch((error) => console.error(error))
+  }
+
+  const deleteData = id => {
+    Alert.alert(
+      "Delete Data",
+      "Dou you really want to delete " + id,
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        {
+          text: "OK", onPress: () => {
+            fetch('http://192.168.43.187:3000/crud/delete/' + id, {
               method: 'DELETE',
             })
-            .then(() => { getData(); showToast() })
-            .catch((error) => console.error(error))            
-          } 
-    }
-    ],
-    { cancelable: false }
-  ); }
+              .then(() => { getData(); showToast() })
+              .catch((error) => console.error(error))
+          }
+        }
+      ],
+      { cancelable: false }
+    );
+  }
 
 
   const showToast = () => {
@@ -230,44 +196,41 @@ const getData = () =>{
     );
   };
 
-  const updateName = text =>{
-    setModalData({...modalData, name: text});
+  const updateName = text => {
+    setModalData({ ...modalData, name: text });
   }
 
 
-
-
-return (
-<View style={styles.container}> 
-{isLoading ? <ActivityIndicator/> : (
+  return (
+    <View style={styles.container}>
+      {isLoading ? <ActivityIndicator /> : (
         <FlatList
           data={data}
-          keyExtractor={(item, index) => 'key'+index}
-          renderItem={({ item,index }) => (
+          keyExtractor={(item, index) => 'key' + index}
+          renderItem={({ item, index }) => (
 
 
-            <TouchableOpacity onPress={()=>openModal(item,index)} key={index} onLongPress={()=>deleteData(item._id)}  delayLongPress={7000}>
-            <Card style={styles.card} >
-              <View style={styles.img}>     
-                  <Image style={styles.tinyLogo} source={{uri: item.path}}/>
-              </View>
-              <View style ={styles.otherDaata}>  
+            <TouchableOpacity onPress={() => openModal(item, index)} key={index} onLongPress={() => deleteData(item._id)} delayLongPress={7000}>
+              <Card style={styles.card} >
+                <View style={styles.img}>
+                  <Image style={styles.tinyLogo} source={{ uri: item.path }} />
+                </View>
+                <View style={styles.otherDaata}>
                   <Text>{item.name}</Text>
                   <Text>{item.designation}</Text>
-              </View> 
-            </Card> 
-          </TouchableOpacity>
+                </View>
+              </Card>
+            </TouchableOpacity>
 
 
-            )}
+          )}
         />
       )}
 
 
-
-<Modal isVisible={modalVisible}
-            onRequestClose={() => setModalVisible(!modalVisible)}
-       testID={'modal'}
+      <Modal isVisible={modalVisible}
+        onRequestClose={() => setModalVisible(!modalVisible)}
+        testID={'modal'}
         backdropColor="#B4B3DB"
         backdropOpacity={0.8}
         animationIn="zoomInDown"
@@ -276,28 +239,22 @@ return (
         animationOutTiming={600}
         backdropTransitionInTiming={600}
         backdropTransitionOutTiming={600}>
-          <View style={styles.modalContainer}>
-            <View style={styles.inputContainer}>
-                <TextInput style={styles.input}  placeholder="Enter Name"  value={modalData.name} onChangeText={updateName}/>
-                <TextInput style={styles.input} placeholder="Enter Designation" value={modalData.designation}  onChangeText={text => setModalData({...modalData, designation:text})}/>
-
-
+        <View style={styles.modalContainer}>
+          <View style={styles.inputContainer}>
+            <TextInput style={styles.input} placeholder="Enter Name" value={modalData.name} onChangeText={updateName} />
+            <TextInput style={styles.input} placeholder="Enter Designation" value={modalData.designation} onChangeText={text => setModalData({ ...modalData, designation: text })} />
             <View >
-                <Btn title="Pick an image from camera roll" onPress={picImage} />
-                {image.uri && <Image source={{ uri: image.uri }} style={{ width: 100, height: 100, marginVertical:10, justifyContent:'space-around' }} />}
+              <Btn title="Pick an image from camera roll" onPress={picImage} />
+              {image.uri && <Image source={{ uri: image.uri }} style={{ width: 100, height: 100, marginVertical: 10, justifyContent: 'space-around' }} />}
             </View>
-
-                <Btn style={styles.btn} title="Submit" color="black" onPress={updateData}/>
-            </View>
+            <Btn style={styles.btn} title="Submit" color="black" onPress={updateData} />
           </View>
+        </View>
       </Modal>
 
     </View>
 
-
-
-
-);
+  );
 };
 
 
@@ -307,60 +264,59 @@ HomeScreen.navigationOptions = {
   header: null,
 };
 
-
-
 const styles = StyleSheet.create({
   container: {
-    width:'100%',
+    width: '100%',
     flex: 1,
     backgroundColor: '#fff',
-    alignItems:'center'
+    alignItems: 'center'
   },
-  card:{
-    flexDirection:'row',
-    height:80,
-    shadowColor:'red',
-    shadowColor:'black',
-    shadowOffset:{width:0, height:2},
+  card: {
+    flexDirection: 'row',
+    height: 80,
+    shadowColor: 'red',
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
     // shadowRadius:6,
-    shadowOpacity:0.26,
-    elevation:5,
-    borderRadius:10,
-    marginLeft:30
+    shadowOpacity: 0.26,
+    elevation: 5,
+    borderRadius: 10,
+    marginLeft: 30
   },
-  img:{
-    alignItems:'flex-start',
-    justifyContent:'flex-start',
-    marginLeft:10
+  img: {
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    marginLeft: 10
   },
-  otherDaata:{
-    alignItems:'flex-start',
-    justifyContent:'flex-start',
+  otherDaata: {
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
     // marginRight:15
   },
   tinyLogo: {
     width: 50,
     height: 50,
   },
-  modalContainer:{
-     flex:1,
-     justifyContent:'space-between',
-     backgroundColor:'#ffffff'
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'space-between',
+    backgroundColor: '#ffffff'
   },
-  inputContainer:{
-    marginTop:20,
-    alignItems:'center'
+  inputContainer: {
+    marginTop: 20,
+    alignItems: 'center'
   },
-  input:{
-    height:30,
-    width:'90%',
-    textAlign:'center',
-    backgroundColor:'#fff',
-    borderBottomColor:'grey',
-    marginVertical:10,
-    padding:10,
-    margin:10
+  input: {
+    height: 30,
+    width: '90%',
+    textAlign: 'center',
+    backgroundColor: '#fff',
+    borderBottomColor: 'grey',
+    marginVertical: 10,
+    padding: 10,
+    margin: 10
   },
 
- 
+
 });
+
